6f4a96b22a1cd46b1af64369cfb10d6e081a515f
ocastudent <145093236+ocastudent@users.noreply.github.com>
2025-06-27 00:59:17 +0000

    3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'users#index'
-  get "users/new",to: "users#new"
+  get 'users/new', to: 'users#new'
   get "users/show"
   get "users/edit"
   post 'users', to: 'users#create'
